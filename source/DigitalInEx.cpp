1440a892711533d783cd249681fbd5111cfd340f
Marcus Chang <marcus.chang@arm.com>
2016-03-11 15:15:55 +0000

    Added DigitalInEx class.

---
+++ source/DigitalInEx.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "wrd-gpio-switch/DigitalInEx.h"
+#include "wrd-gpio-switch/GPIOSwitch.h"
+
+DigitalInEx::DigitalInEx(uint32_t _pin, uint32_t _location)
+    :   pin(_pin),
+        location(_location),
+        cache(0),
+        local(NULL)
+{
+    if (location)
+    {
+        GPIOSwitch::setDirection(pin, location, 0);
+    }
+    else
+    {
+        local = SharedPointer<DigitalIn>(new DigitalIn((PinName) pin));
+    }
+}
+
+DigitalInEx::DigitalInEx(uint32_t _pin, uint32_t _location, PinMode mode)
+    :   pin(_pin),
+        location(_location),
+        cache(0),
+        local(NULL)
+{
+    if (location)
+    {
+        GPIOSwitch::setDirection(pin, location, 0);
+    }
+    else
+    {
+        local = SharedPointer<DigitalIn>(new DigitalIn((PinName) pin, mode));
+    }
+}
+
+int DigitalInEx::read(void)
+{
+    if (location)
+    {
+        // schedule cache to be updated
+        FunctionPointer1<void, int> fp(this, &DigitalInEx::updateCache);
+        GPIOSwitch::readInput(pin, location, fp);
+
+        printf("ccccc: %d\r\n", cache);
+
+        return cache;
+    }
+    else
+    {
+        return local->read();
+    }
+}
+
+void DigitalInEx::read(FunctionPointer1<void, int> callback)
+{
+    if (location)
+    {
+        GPIOSwitch::readInput(pin, location, callback);
+    }
+    else
+    {
+        minar::Scheduler::postCallback(callback.bind(local->read()))
+            .tolerance(1);
+    }
+}
+
+void DigitalInEx::mode(PinMode pull)
+{
+    if (location)
+    {
+        // not implemented
+    }
+    else
+    {
+        local->mode(pull);
+    }
+}
+
+void DigitalInEx::updateCache(int value)
+{
+    printf("update: %d\r\n", value);
+    cache = value;
+}
--- wrd-gpio-switch/DigitalInEx.h
+++ wrd-gpio-switch/DigitalInEx.h
@@ -18,10 +18,85 @@
 #ifndef __WRD_GPIO_DIGITAL_IN_EX_H__
 #define __WRD_GPIO_DIGITAL_IN_EX_H__
 
+#include "mbed-drivers/mbed.h"
+#include "core-util/SharedPointer.h"
+
+using namespace mbed::util;
+
 class DigitalInEx
 {
 public:
-    DigitalInEx() {}
+    /**
+     * @brief Create a DigitalInEx connected to the specified pin.
+     * @details The optional location specifies which IC the I/O pin is on.
+     *          The main MCU is location 0. For I2C I/O expanders, the location
+     *          is the I2C address.
+     *
+     * @param pin digital pin to connect to
+     * @param location digital pin location
+     * @param mode the initial mode of the pin
+     */
+    DigitalInEx(uint32_t pin, uint32_t location = 0);
+
+    /**
+     * @brief Create a DigitalInEx connected to the specified pin.
+     * @details The optional location specifies which IC the I/O pin is on.
+     *          The main MCU is location 0. For I2C I/O expanders, the location
+     *          is the I2C address.
+     *
+     * @param pin digital pin to connect to
+     * @param location digital pin location
+     * @param mode the initial mode of the pin
+     */
+    DigitalInEx(uint32_t pin, uint32_t location, PinMode mode);
+
+    /**
+     * @brief Read the input, represented as 0 or 1 (int)
+     * @details For inputs on external I/O devices, a cached value is returned
+     *          immediately and a new value is fetched into the cache.
+     *          When possible, the asynchronous API should be used.
+     *
+     * @return An integer representing the state of the input pin,
+     *         0 for logical 0, 1 for logical 1
+     */
+    int read(void);
+
+    /**
+     * @brief Read the input, represented as 0 or 1 (int)
+     * @details The value is returned in the callback function.
+     *          This function call reads the newest value from external I/O
+     *          devices.
+     *
+     * @param callback [description]
+     */
+    void read(FunctionPointer1<void, int> callback);
+
+    /**
+     * @brief Set the input pin mode.
+     * @param pull PullUp, PullDown, PullNone, OpenDrain
+     */
+    void mode(PinMode pull);
+
+#ifdef MBED_OPERATORS
+    /**
+     * @brief And operator shorthand for read()
+     * @return An integer representing the state of the input pin,
+     *         0 for logical 0, 1 for logical 1
+     */
+    operator int()
+    {
+        return read();
+    }
+#endif
+
+private:
+    void updateCache(int);
+
+    uint32_t pin;
+    uint32_t location;
+    int cache;
+
+    SharedPointer<DigitalIn> local;
 };
 
 #endif // __WRD_GPIO_DIGITAL_IN_EX_H__
