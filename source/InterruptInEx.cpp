2c89af607ccfcd8a47fb9d9e868b86503565cb30
Marcus Chang <marcus.chang@arm.com>
2016-03-11 15:16:16 +0000

    Added InterruptInEx class.

--- source/InterruptInEx.cpp
+++ source/InterruptInEx.cpp
@@ -16,11 +16,128 @@
  */
 
 #include "wrd-gpio-switch/InterruptInEx.h"
+#include "wrd-gpio-switch/GPIOSwitch.h"
 
 InterruptInEx::InterruptInEx(uint32_t _pin, uint32_t _location)
     :   pin(_pin),
-        location(_location)
+        location(_location),
+        value(0)
 {
-
+    if ((location == 0) && ((PinName) pin != NC))
+    {
+        local = SharedPointer<InterruptIn>(new InterruptIn((PinName) pin));
+    }
 }
 
+int InterruptInEx::read()
+{
+    if (location)
+    {
+        return value;
+    }
+    else
+    {
+        return local->read();
+    }
+}
+
+void InterruptInEx::internalRise(FunctionPointer0<void> callback)
+{
+    // location always non-zero
+
+    // enable interrupts if not previously enabled
+    if ((riseCallback == false) && (fallCallback == false))
+    {
+        FunctionPointer1<void, int> fp(this, &InterruptInEx::interruptHandler);
+        GPIOSwitch::enableInterrupt(pin, location, fp);
+    }
+
+    // store callback
+    riseCallback = callback;
+
+    // disable interrupts if no longer enabled
+    if ((riseCallback == false) && (fallCallback == false))
+    {
+        GPIOSwitch::disableInterrupt(pin, location);
+    }
+}
+
+void InterruptInEx::internalFall(FunctionPointer0<void> callback)
+{
+    // location always non-zero
+
+    // enable interrupts if not previously enabled
+    if ((riseCallback == false) && (fallCallback == false))
+    {
+        FunctionPointer1<void, int> fp(this, &InterruptInEx::interruptHandler);
+        GPIOSwitch::enableInterrupt(pin, location, fp);
+    }
+
+    // store callback
+    fallCallback = callback;
+
+    // disable interrupts if no longer enabled
+    if ((riseCallback == false) && (fallCallback == false))
+    {
+        GPIOSwitch::disableInterrupt(pin, location);
+    }
+}
+
+void InterruptInEx::mode(PinMode pull)
+{
+    if (location)
+    {
+        // not supported
+    }
+    else
+    {
+        local->mode(pull);
+    }
+}
+
+void InterruptInEx::enable_irq()
+{
+    if (location)
+    {
+        FunctionPointer1<void, int> fp(this, &InterruptInEx::interruptHandler);
+        GPIOSwitch::enableInterrupt(pin, location, fp);
+    }
+    else
+    {
+        local->enable_irq();
+    }
+}
+
+void InterruptInEx::disable_irq()
+{
+    if (location)
+    {
+        GPIOSwitch::disableInterrupt(pin, location);
+    }
+    else
+    {
+        local->disable_irq();
+    }
+}
+
+void InterruptInEx::interruptHandler(int _value)
+{
+    printf("%d\r\n", _value);
+
+    value = _value;
+
+    if (value == 0)
+    {
+        if (fallCallback)
+        {
+            fallCallback.call();
+        }
+    }
+    else
+    {
+        if (riseCallback)
+        {
+            riseCallback.call();
+        }
+    }
+}
--- wrd-gpio-switch/InterruptInEx.h
+++ wrd-gpio-switch/InterruptInEx.h
@@ -19,7 +19,7 @@
 #define __WRD_GPIO_INTERRUPT_IN_EX_H__
 
 #include "mbed-drivers/mbed.h"
-#include "wrd-gpio-switch/GPIOSwitch.h"
+#include "core-util/SharedPointer.h"
 
 using namespace mbed::util;
 
@@ -28,9 +28,128 @@ class InterruptInEx
 public:
     InterruptInEx(uint32_t pin, uint32_t location = 0);
 
+    /**
+     * @brief Read pin value.
+     * @return Pin value, high 1, low 0
+     */
+    int read();
+#ifdef MBED_OPERATORS
+    operator int()
+    {
+        return read();
+    }
+#endif
+
+    /**
+     * @brief Attach a function to call when a rising edge occurs on the input
+     *
+     * @param callback A pointer to a void function, or 0 to set as none
+     */
+    void rise(void (*callback)(void))
+    {
+        if (location)
+        {
+            FunctionPointer0<void> fp(callback);
+            internalRise(callback);
+        }
+        else
+        {
+            local->rise(callback);
+        }
+    }
+
+    /**
+     * @brief Attach a member function to call when a rising edge occurs on the input
+     *
+     * @param object pointer to the object to call the member function on
+     * @param member pointer to the member function to be called
+     */
+    template<typename T>
+    void rise(T* object, void (T::*member)(void))
+    {
+        if (location)
+        {
+            FunctionPointer0<void> fp(object, member);
+            internalRise(fp);
+        }
+        else
+        {
+            local->rise(object, member);
+        }
+    }
+
+    /**
+     * @brief Attach a function to call when a falling edge occurs on the input
+     *
+     * @param callback A pointer to a void function, or 0 to set as none
+     */
+    void fall(void (*callback)(void))
+    {
+        if (location)
+        {
+            FunctionPointer0<void> fp(callback);
+            internalFall(fp);
+        }
+        else
+        {
+            local->fall(callback);
+        }
+    }
+
+    /**
+     * @brief Attach a member function to call when a falling edge occurs on the input
+     *
+     * @param object pointer to the object to call the member function on
+     * @param member pointer to the member function to be called
+     */
+    template<typename T>
+    void fall(T* object, void (T::*member)(void))
+    {
+        if (location)
+        {
+            FunctionPointer0<void> fp(object, member);
+            internalFall(fp);
+        }
+        else
+        {
+            local->fall(object, member);
+        }
+    }
+
+    /**
+     * @brief Enable pullup or pulldown.
+     *
+     * @param pull mode PullUp, PullDown, PullNone
+     */
+    void mode(PinMode pull);
+
+    /**
+     * @brief Enable IRQ.
+     * @details This method depends on hw implementation, might enable one
+     *          port interrupts.
+     */
+    void enable_irq();
+
+    /**
+     * @brief Disable IRQ.
+     * @details This method depends on hw implementation, might disable one
+     *          port interrupts.
+     */
+    void disable_irq();
+
+
 private:
+    void internalRise(FunctionPointer0<void>);
+    void internalFall(FunctionPointer0<void>);
+
+    void interruptHandler(int);
+
     uint32_t pin;
     uint32_t location;
+    SharedPointer<InterruptIn> local;
+    int value;
+    FunctionPointer0<void> riseCallback;
+    FunctionPointer0<void> fallCallback;
 };
 
 #endif // __WRD_GPIO_INTERRUPT_IN_EX_H__
